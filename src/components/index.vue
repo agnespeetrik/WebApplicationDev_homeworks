6698219aad0b7707286685f01fc3f224691423da
Agnespeetrik <agneskeerdo@gmail.com>
2020-11-22 17:41:31 +0200

    posts and nav

--- src/components/index.vue
+++ src/components/index.vue
@@ -3,11 +3,31 @@
 </template>
 
 <script>
+import navar from './navbar';
+import posts from './posts';
+
 export default {
-name: "index.vue"
+name: "index",
+components: {
+    navbar,
+    posts
+  }
 }
 </script>
 
 <style scoped>
-
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
---
+++ src/components/posts.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <section class="main-container" v-bind:key="post.id" v-for="post in posts">
+      <Post v-bind:post="post" />
+    </section>
+  </div>
+</template>
+
+<script>
+import Post from './Post';
+import axios from 'axios';
+
+export default {
+  name: 'Posts',
+  components: {
+    Post
+  },
+  data(){
+    return {
+      posts: []
+    }
+  },
+  created(){
+    axios.get('https://private-517bb-wad20postit.apiary-mock.com/posts')
+      .then(res => this.posts = res.data)
+      .catch(err => console.log(err));
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+</style>
