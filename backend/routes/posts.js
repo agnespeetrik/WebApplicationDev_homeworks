88b2b4168433d5bccf1a798d21935d7ad0c40951
Agnespeetrik <agneskeerdo@gmail.com>
2020-12-11 14:04:58 +0200

    tried to do task1, task2, task3, but something is still wrong

--- backend/routes/posts.js
+++ backend/routes/posts.js
@@ -22,22 +22,65 @@ router.get('/', authorize, (request, response) => {
 
 });
 
-router.post('/', authorize,  (request, response) => {
+router.post('/', authorize, (request, response) => {
 
-    // Endpoint to create a new post
+    // endpoint to create a new post
 
+    let params = {
+        userId: request.currentUser.id,
+        text: request.body.text,
+        media: {
+            type: request.body.media.type,
+            url: request.body.media.url,
+        },
+    };
+
+    const fieldMissing = {
+        code: null,
+        message: 'Please provide all required information.'
+    };
+
+    for (let field in form) {
+        // if there is no text OR no url (if type is) OR no type (if url is)
+        if (!params.text || !params.media.url && params.media.type || params.media.url && !params.media.type) {
+            response.json(fieldMissing, 400);
+            return;
+        }
+    }
+
+    PostModel.create(params, () => {
+        response.status(201).json()
+        return;
+    });
 });
 
 
 router.put('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to like a post
+
+    if(request.params.postId){
+        PostModel.like(request.currentUser.id, request.params.postId, () => {
+            response.status(200).json()
+        })}
+        else{
+            response.status(404).json(noPost)
+        }
 });
 
 router.delete('/:postId/likes', authorize, (request, response) => {
 
     // Endpoint for current user to unlike a post
 
+    if(request.params.postId){
+        PostModel.unlike(request.currentUser.id, request.params.postId, ()=>{
+            response.status(200).json()
+        })
+    }
+    else{
+        //post not found
+        response.status(404).json(noPost)
+    }
 });
 
 module.exports = router;
